fb597469ab5abaa151a517db42a73a84e0c77e51
Go Coder <83633399+gocoder-ai@users.noreply.github.com>
2022-04-07 07:53:42 +0800

    Update mail.go
    Fixed hasSPF

--- mail.go
+++ mail.go
@@ -43,7 +43,7 @@ func checkDom(domain string)  {
 	}
 
 	for _, record := range txtRecords {
-		if strings.HasPrefix(record, "v=DMARC1"){
+		if strings.HasPrefix(record, "v=spf"){
 			hasSPF = true
 			sprRecord = record
 			break
