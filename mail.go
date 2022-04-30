795deea6fb6fd1e47fa64842716a32d5cfe48d81
GOCrazy <83633399+gocrazygh@users.noreply.github.com>
2022-04-30 20:10:42 +0800

    Create mail.go

---
+++ mail.go
@@ -0,0 +1,52 @@
+package mail
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func CheckDom(domain string)  {
+	var hasMX, hasSPF, hasDMARC bool
+	var sprRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf"){
+			hasSPF = true
+			sprRecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1"){
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%v, %v, %v, %v, %v, %v", domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord)
+}
