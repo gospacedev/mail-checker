ec3fa06f4da03109de2f6e8b0d1bdaf0ea3a38e5
Grantley Cullar <83633399+gocrazygt@users.noreply.github.com>
2022-08-30 12:22:49 +0800

    Added json format to mail.go

--- mail.go
+++ mail.go
@@ -1,17 +1,35 @@
 package mail
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net"
 	"strings"
 )
-// CheckDom takes a domain name as a string, and returns a comma separated string with the domain name,
-// whether it has an MX record, whether it has an SPF record, the SPF record, whether it has a DMARC
-// record, and the DMARC record
-func CheckDom(domain string) {
-	var hasMX, hasSPF, hasDMARC bool
-	var sprRecord, dmarcRecord string
+
+// `DomMailInfo` is a struct with fields `Domain`, `HasMX`, `HasSPF`, `HasDMARC`, `SPRRecord`, and
+// `DMARCRecord`.
+// @property {string} Domain - The domain name
+// @property {bool} HasMX - Does the domain have an MX record?
+// @property {bool} HasSPF - Does the domain have an SPF record?
+// @property {bool} HasDMARC - Does the domain have a DMARC record?
+// @property {string} SPRRecord - The SPF record for the domain
+// @property {string} DMARCRecord - The DMARC record for the domain.
+type DomMailInfo struct {
+	Domain string `json:"Domain"`
+	HasMX bool `json:"HasMX"`
+	HasSPF bool `json:"HasSPF"`
+	HasDMARC bool `json:"HasDMARC"`
+	SPRRecord string `json:"SPRRecord"`
+	DMARCRecord string `json:"DMARCRecord"`
+}
+
+// CheckDomainMX takes a domain name as a string, and returns a JSON string containing the domain name,
+// whether the domain has MX records, whether the domain has an SPF record, whether the domain has a
+// DMARC record, the SPF record, and the DMARC record
+func CheckDomainMX(domain string) string {
+	var HasMX, HasSPF, HasDMARC bool
+	var SPRRecord, DMARCRecord string
 
 	mxRecords, err := net.LookupMX(domain)
 
@@ -20,7 +38,7 @@ func CheckDom(domain string) {
 	}
 
 	if len(mxRecords) > 0 {
-		hasMX = true
+		HasMX = true
 	}
 
 	txtRecords, err := net.LookupTXT(domain)
@@ -31,24 +49,30 @@ func CheckDom(domain string) {
 
 	for _, record := range txtRecords {
 		if strings.HasPrefix(record, "v=spf") {
-			hasSPF = true
-			sprRecord = record
+			HasSPF = true
+			SPRRecord = record
 			break
 		}
 	}
 
-	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	DMARCRecords, err := net.LookupTXT("_dmarc." + domain)
 	if err != nil {
 		log.Printf("Error: %v\n", err)
 	}
 
-	for _, record := range dmarcRecords {
+	for _, record := range DMARCRecords {
 		if strings.HasPrefix(record, "v=DMARC1") {
-			hasDMARC = true
-			dmarcRecord = record
+			HasDMARC = true
+			DMARCRecord = record
 			break
 		}
 	}
 
-	fmt.Printf("%v, %v, %v, %v, %v, %v", domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord)
+	DomMailInfoNew := DomMailInfo{domain, HasMX, HasSPF, HasDMARC, SPRRecord, DMARCRecord}
+	DomMailInfoNewJson, err := json.MarshalIndent(DomMailInfoNew, " ", "    ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(DomMailInfoNewJson)
 }
